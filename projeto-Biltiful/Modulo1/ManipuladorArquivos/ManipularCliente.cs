95ac001e7fa65c9fb8e33c665d2a291613bf8e9b
gutsserrano <gutsserrano@gmail.com>
2024-05-09 16:42:43 -0300

    Criando nome dos métodos e classes

---
+++ projeto-Biltiful/Modulo1/ManipuladorArquivos/ManipularCliente.cs
@@ -0,0 +1,42 @@
+﻿using projeto_Biltiful.Modulo1.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_Biltiful.Modulo1.ManipuladorArquivos
+{
+    internal class ManipularCliente
+    {
+        string Caminho { get; set; }
+        string Arquivo {  get; set; }
+
+        public ManipularCliente(string caminho, string arquivo)
+        {
+            this.Caminho = caminho;
+            this.Arquivo = arquivo;
+        }
+
+        public List<Cliente> Recuperar(string caminho, string arquivo)
+        {
+            // TODO
+            return null;
+        }
+
+        public void Salvar(List<Cliente> clientes)
+        {
+            // TODO
+        }
+
+        public void Cadastrar()
+        {
+            // Recebe os dados dos clientes e adiciona eles na lista
+        }
+
+        public void Editar()
+        {
+            // Edita um Cliente específico
+        }
+    }
+}
